6a6ffb6ef5934de4b621237ad223f83ea3ef4112
Chea Bonnak <chea.bonnak@gmail.com>
2016-06-01 23:09:25 +0700

    Design create user form

--- resources/views/auth/user.blade.php
+++ resources/views/auth/user.blade.php
@@ -11,11 +11,51 @@
 		<div class="col-md-12">
 			<div class="panel panel-default">
 				<div class="panel-body">
-					<user-grid
+					<!-- <user-grid
 						:users="users"
 						:pagination="pagination"
-					></user-grid>
-					@{{ message }}
+					></user-grid> -->
+					
+					<form class="form-horizontal">
+						<div class="form-group">
+                <label class="col-md-3 col-xs-12 control-label">User Account</label>
+                <div class="col-md-6 col-xs-12">                                            
+                    <div>
+                        <input type="text" class="form-control"/>
+                    </div>                                            
+                    <span class="help-block">This is sample of text field</span>
+                </div>
+            </div>
+            <div class="form-group">                                        
+              <label class="col-md-3 col-xs-12 control-label">Password</label>
+              <div class="col-md-6 col-xs-12">
+                  <div>
+                      <input type="password" class="form-control"/>
+                  </div>            
+                  <span class="help-block">Password field sample</span>
+              </div>
+          	</div>
+            <div class="form-group">
+                <label class="col-md-3 col-xs-12 control-label">Full Name</label>
+                <div class="col-md-6 col-xs-12">                                            
+                    <div>
+                        <input type="text" class="form-control"/>
+                    </div>                                            
+                    <span class="help-block">This is sample of text field</span>
+                </div>
+            </div>
+            <div class="form-group">
+	            <label class="col-md-3 col-xs-12 control-label">Role</label>
+	            <div class="col-md-6 col-xs-12">                                                                                            
+	                <select class="form-control select">
+	                    <option value="1">Adminstrator</option>
+	                    <option value="2">Accountant</option>
+	                </select>
+	                <span class="help-block">Select box example</span>
+	            </div>
+	        	</div>
+					</form>
+
 				</div>
 			</div>
 		</div>
